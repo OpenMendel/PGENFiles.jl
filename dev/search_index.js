9b58e69d2bbcd4360d02957dd425c96ac5b35ae4
Documenter.jl <documenter@juliadocs.github.io>
2022-01-12 19:03:39 +0000

    build based on 82a13e5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PGEN.jl","page":"PGEN.jl","title":"PGEN.jl","text":"","category":"section"},{"location":"","page":"PGEN.jl","title":"PGEN.jl","text":"Routines for reading compressed storage of genotyped or imputed markers in Plink 2","category":"page"}]
+}
